889a3219da1f758576a494f14af659b74de5e0a9
DaikiOnodera <daikionodera0101@gmail.com>
2023-12-06 09:06:41 +0900

    first

---
+++ test/CMakeFiles/cmake_tutorial_1.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/daikionodera/Playground/cmake-test/test/tutorial_1.cc" "CMakeFiles/cmake_tutorial_1.dir/tutorial_1.cc.o" "gcc" "CMakeFiles/cmake_tutorial_1.dir/tutorial_1.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ test/CMakeFiles/cmake_tutorial_1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmake_tutorial_1.dir/tutorial_1.cc.o"
+  "CMakeFiles/cmake_tutorial_1.dir/tutorial_1.cc.o.d"
+  "cmake_tutorial_1"
+  "cmake_tutorial_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmake_tutorial_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
